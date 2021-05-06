6dc7e4b2b1a766ddb82860563f0badb9db076036
Deep Bag <43214783+deepbag@users.noreply.github.com>
2021-05-06 23:29:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 // import 'firebase/storage';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBM_0Sbl5VDK2Grb4mB8dlPQOQOm-XHk3M",
-    authDomain: "onereaderio-aa29b.firebaseapp.com",
-    projectId: "onereaderio-aa29b",
-    storageBucket: "onereaderio-aa29b.appspot.com",
-    messagingSenderId: "356568747920",
-    appId: "1:356568747920:web:a2c2ce066afce88c6fad15",
-    measurementId: "G-06YK8RVKDK"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 });
 
 const db = firebaseApp.firestore();
